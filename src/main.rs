aa1c8829b48a15ffb1e690283caca72ecc7bb7cd
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2024-02-27 22:21:14 +0800

    feat: 🔊 add env_logger for logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use env_logger::Env;
+use log::{debug, info};
 use tokio::fs::read;
 
 use crate::cli::Args;
@@ -6,12 +8,19 @@ mod cli;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::Builder::from_env(
+        Env::new()
+            .filter_or("RSJUDGE_LOG", "info")
+            .write_style("RSJUDGE_LOG_STYLE"),
+    )
+    .try_init()?;
+
     let args = Args::try_parse()?;
-    println!("{:?}", args);
+    info!("{:?}", args);
 
     let config = read(args.config_dir.join("executors.toml")).await?;
 
-    println!(
+    debug!(
         "Config:\n{:#?}",
         String::from_utf8_lossy(&config).parse::<toml::Value>()
     );
