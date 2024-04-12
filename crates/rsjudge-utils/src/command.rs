d1f07ae6e5e103ce1b5de991948e4176e3ffa39d
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2024-04-12 17:09:29 +0800

    feat(rsjudge-utils): 🥅 use custom error type for `check_output`

--- crates/rsjudge-utils/src/command.rs
+++ crates/rsjudge-utils/src/command.rs
@@ -3,12 +3,12 @@
 //! Functions for working with [`std::process::Command`].
 
 use std::{
-    io::ErrorKind,
+    io::{self, ErrorKind},
     iter,
-    process::{Command, Output},
+    process::{Command, Output, Stdio},
 };
 
-use anyhow::{bail, ensure};
+use thiserror::Error;
 
 /// Display a command in a human-readable format, suitable for error messages.
 ///
@@ -30,6 +30,31 @@ pub fn display_cmd(cmd: &Command) -> String {
     shell_words::join(args)
 }
 
+/// Error type for failed command execution.
+#[derive(Debug, Error)]
+pub enum ExecutionError {
+    /// The requested command was not found.
+    #[error("Program `{program}` not found. Please install it or check your PATH.")]
+    NotFound {
+        /// The command name.
+        program: String,
+    },
+
+    /// The command failed with an error.
+    #[error("Command failed: {0}")]
+    Failed(#[from] io::Error),
+
+    /// The command failed with a non-zero exit status.
+    #[error("Command `{command}` failed with: {:?}", .output)]
+    /// Error type for a non-zero exit status.
+    NonZeroExitStatus {
+        /// The command that failed.
+        command: String,
+        /// The output of the command.
+        output: Output,
+    },
+}
+
 /// Run a command, returning the output if succeeded, with some error handling.
 ///
 /// # Examples
@@ -43,24 +68,55 @@ pub fn display_cmd(cmd: &Command) -> String {
 /// let output = check_output(&mut cmd).unwrap();
 /// assert_eq!(output.stdout, b"Hello, world!\n");
 /// ```
-pub fn check_output(cmd: &mut Command) -> anyhow::Result<Output> {
-    let output = match cmd.output() {
-        Ok(o) => o,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => bail!(
-                "`{}` not found. Please install it or check your PATH.",
-                cmd.get_program().to_string_lossy()
-            ),
-            _ => Err(e)?,
-        },
-    };
+pub fn check_output(cmd: &mut Command) -> Result<Output, ExecutionError> {
+    let child = cmd
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| match e.kind() {
+            ErrorKind::NotFound => ExecutionError::NotFound {
+                program: cmd.get_program().to_string_lossy().into_owned(),
+            },
+            _ => e.into(),
+        })?;
 
-    ensure!(
-        output.status.success(),
-        "`{:?}` failed with: {:#?}",
-        display_cmd(cmd),
-        output
-    );
+    let output = child.wait_with_output()?;
 
-    Ok(output)
+    if !output.status.success() {
+        Err(ExecutionError::NonZeroExitStatus {
+            command: display_cmd(cmd),
+            output,
+        })
+    } else {
+        Ok(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::command::ExecutionError;
+
+    #[test]
+    fn command_not_found_with_custom_error() {
+        let mut cmd = std::process::Command::new("nonexistent");
+        let err = super::check_output(&mut cmd).unwrap_err();
+        assert!(matches!(err, ExecutionError::NotFound { .. }));
+    }
+
+    #[test]
+    fn command_failed_with_custom_error() {
+        let mut cmd = std::process::Command::new("false");
+        let err = super::check_output(&mut cmd).unwrap_err();
+
+        assert!(matches!(err, ExecutionError::NonZeroExitStatus { .. }));
+    }
+
+    #[test]
+    fn capture_output() {
+        let mut cmd = std::process::Command::new("echo");
+        cmd.arg("Hello, world!");
+        let output = super::check_output(&mut cmd).unwrap();
+        assert_eq!(output.stdout, b"Hello, world!\n");
+    }
 }
