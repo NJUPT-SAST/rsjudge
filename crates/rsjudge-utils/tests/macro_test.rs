ca593169f993128aa61fa684362557e2ae49c3ea
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2024-06-07 11:12:48 +0800

    test(rsjudge-utils): :white_check_mark: normalize tests

--- crates/rsjudge-utils/tests/macro_test.rs
+++ crates/rsjudge-utils/tests/macro_test.rs
@@ -5,5 +5,5 @@ use trybuild::TestCases;
 #[test]
 fn tests() {
     let test_cases = TestCases::new();
-    test_cases.compile_fail("tests/logging_on_non_result.test");
+    test_cases.compile_fail("tests/cases/logging_on_non_result.rs");
 }
