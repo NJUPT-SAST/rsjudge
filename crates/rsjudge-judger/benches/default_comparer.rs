1820a95ba10ba7d7cf90356fc2f7f788ece255da
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2024-06-10 16:30:02 +0800

    feat(rsjudge-judger): ✅ update benchmark

--- crates/rsjudge-judger/benches/default_comparer.rs
+++ crates/rsjudge-judger/benches/default_comparer.rs
@@ -8,9 +8,8 @@ use tokio::{fs::File, runtime::Runtime};
 
 fn bench(c: &mut Criterion) {
     let data_dir = Path::new("./benches/data");
-    let data_file = data_dir.join("100M");
-    let data_file_trimmed = data_dir.join("100M.trim");
-    const MB: u64 = 1024 * 1024;
+    const KILO: u64 = 1024;
+    const MEGA: u64 = 1024 * 1024;
     const COMPARERS: &[(DefaultComparer, &str)] = &[
         (DefaultComparer::common(), "common"),
         (DefaultComparer::exact_match(), "exact-match"),
@@ -22,22 +21,37 @@ fn bench(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("DefaultComparer");
     group.sampling_mode(SamplingMode::Flat);
+    for (size_str, bytes) in [
+        ("1k", KILO),
+        ("16k", 16 * KILO),
+        ("256k", 256 * KILO),
+        ("4M", 4 * MEGA),
+        ("64M", 64 * MEGA),
+    ] {
+        let data_file = data_dir.join(size_str);
 
-    for (comparer, id) in COMPARERS {
-        group.throughput(Throughput::Bytes(100 * MB));
-        group.bench_with_input(BenchmarkId::from_parameter(id), comparer, |b, comparer| {
-            b.to_async(Runtime::new().unwrap()).iter(|| async {
-                let result = black_box(comparer)
-                    .compare(
-                        File::open(black_box(&data_file)).await?,
-                        File::open(black_box(&data_file_trimmed)).await?,
-                    )
-                    .await?;
+        let data_file_trimmed = data_dir.join(format!("{}.trim", size_str));
+        for (comparer, id) in COMPARERS {
+            group.throughput(Throughput::Bytes(bytes));
+            group.bench_with_input(
+                BenchmarkId::new(*id, size_str),
+                &(data_file.as_path(), data_file_trimmed.as_path()),
+                |b, (data_file, data_file_trimmed)| {
+                    b.to_async(Runtime::new().unwrap()).iter(|| async {
+                        let result = black_box(comparer)
+                            .compare(
+                                File::open(black_box(data_file)).await?,
+                                File::open(black_box(data_file_trimmed)).await?,
+                            )
+                            .await?;
 
-                Ok::<_, io::Error>(result)
-            });
-        });
+                        Ok::<_, io::Error>(result)
+                    });
+                },
+            );
+        }
     }
+    group.finish();
 }
 
 criterion_group!(benches, bench);
