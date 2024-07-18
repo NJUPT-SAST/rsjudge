b89b27855cd292f1ac4340aaa9fed53f080cce3b
Jisu-Woniu <31986081+Jisu-Woniu@users.noreply.github.com>
2024-07-18 10:24:53 +0800

    test(rsjudge-utils): 🚚 replace trybuild with doctests

--- crates/rsjudge-utils/src/error_macros.rs
+++ crates/rsjudge-utils/src/error_macros.rs
@@ -24,3 +24,27 @@ macro_rules! log_if_error {
         })
     };
 }
+
+/// ```compile_fail
+/// use std::fmt::Display;
+///
+/// use rsjudge_utils::log_if_error;
+///
+/// #[derive(Debug)]
+/// struct S;
+/// impl Display for S {
+///     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+///         write!(f, "S")
+///     }
+/// }
+///
+/// impl S {
+///     fn inspect_err<F: FnOnce(Self) -> Self>(self, f: F) -> Self {
+///         f(self)
+///     }
+/// }
+///
+/// let _ = log_if_error!(S);
+/// ```
+#[cfg(doctest)]
+pub struct LoggingOnNonResult;
--- crates/rsjudge-utils/tests/macro_test.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-use trybuild::TestCases;
-
-#[test]
-fn tests() {
-    let test_cases = TestCases::new();
-    test_cases.compile_fail("tests/cases/logging_on_non_result.rs");
-}
