d797f3f0c36a82e4ff19e6e5f431eb6888b3a7d3
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2025-01-02 19:16:06 +0800

    style(rsjudge-utils): 🎨 simplify code structure

--- crates/rsjudge-utils/src/lib.rs
+++ crates/rsjudge-utils/src/lib.rs
@@ -8,8 +8,27 @@
 #[macro_use]
 mod error_macros;
 
-mod trim;
-
-pub use trim::{trim_space, trim_space_end, trim_space_start};
+/// Returns a byte slice with trailing ASCII Space bytes removed.
+///
+/// ASCII Space refers to the byte `0x20`.
+///
+/// # Examples
+///
+/// ```
+/// use rsjudge_utils::trim_space_end;
+/// assert_eq!(trim_space_end(b"\r hello world\n "), b"\r hello world\n");
+/// assert_eq!(trim_space_end(b"  "), b"");
+/// assert_eq!(trim_space_end(b""), b"");
+/// ```
+#[inline]
+#[must_use = "This function does not modify the input."]
+pub const fn trim_space_end(mut bytes: &[u8]) -> &[u8] {
+    // Note: A pattern matching based approach (instead of indexing) allows
+    // making the function const.
+    while let [rest @ .., b' '] = bytes {
+        bytes = rest;
+    }
+    bytes
+}
 
 pub mod command;
