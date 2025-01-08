656bfda7744354fd653d0c856613dc1ca6c53ae6
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2025-01-08 23:45:30 +0800

    build(xtask): ➖ remove sh crate from xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{env::set_current_dir, path::Path};
+use std::{env::set_current_dir, io, path::Path, process::Command};
 
 use clap::{Parser, ValueEnum};
-use sh::cmd;
 
 #[derive(Debug, ValueEnum, Clone, Copy, PartialEq, Eq)]
 /// Package distribution-specific packages.
@@ -17,12 +16,12 @@ enum Package {
 #[derive(Debug, Parser)]
 #[clap(about, long_about)]
 /// Build related tasks.
-enum Command {
+enum Args {
     /// Package distribution-specific packages.
     Dist {
         /// Which package to build.
         #[arg(value_enum)]
-        package: Package,
+        pkg: Package,
     },
     /// Build Docker image.
     Docker,
@@ -31,30 +30,28 @@ enum Command {
     Debug,
 }
 
+fn exec(program: &str, args: &[&str]) -> io::Result<()> {
+    Command::new(program).args(args).spawn()?.wait()?;
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
-    let command = Command::parse();
+    let args = Args::parse();
 
     // chdir to the workspace root so that `cargo xtask` can be invoked from anywhere.
     set_current_dir(Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap())?;
 
-    match command {
-        Command::Dist { package } => match package {
-            Package::Deb => cmd!(cargo deb "-v" "--locked"),
-            Package::Rpm => cmd! {
-                cargo build "--release" "--locked";
-                cargo "generate-rpm"
-            },
-        },
-        Command::Docker => cmd!(docker build "-t" rsjudge "."),
-
+    match args {
+        Args::Dist { pkg: Package::Deb } => exec("cargo", &["deb", "-v", "--locked"])?,
+        Args::Dist { pkg: Package::Rpm } => {
+            // `cargo-generate-rpm` does not invoke `cargo build` itself.
+            exec("cargo", &["build", "--release", "--locked"])?;
+            exec("cargo", &["generate-rpm"])?;
+        }
+        Args::Docker => exec("docker", &["build", "-t", "rsjudge", "."])?,
         #[cfg(feature = "dbg")]
-        Command::Debug => return Ok(()),
+        Args::Debug => {}
     }
-    .try_for_each(|cmd| {
-        #[cfg(feature = "dbg")]
-        eprintln!("Executing: {:?}", cmd);
-        cmd.exec()
-    })?;
 
     Ok(())
 }
