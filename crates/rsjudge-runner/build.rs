efe6aa54f6c777c47f57f3eb39f88280c3ce95da
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2025-04-03 22:22:06 +0800

    build(msrv): ⬆️ bump MSRV to 1.84
    Features from Rust 1.84 can be used now.

--- crates/rsjudge-runner/build.rs
+++ crates/rsjudge-runner/build.rs
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
-    if rustversion::cfg!(since(1.80)) {
-        println!("cargo:rustc-check-cfg=cfg(feature, values(\"setgroups\"))");
-    }
+    println!("cargo:rustc-check-cfg=cfg(feature, values(\"setgroups\"))");
+
     if rustversion::cfg!(nightly) {
         println!("cargo:rustc-cfg=feature=\"setgroups\"");
     }
